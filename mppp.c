60f74430ceb89fa6b614787790bd322b825e8bca
dongwi <52236474@qq.com>
2018-09-14 04:38:46 +0800

    [internal] 添加部分实现，目前还编译不通过

--- mppp.c
+++ mppp.c
@@ -1,8 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef unsigned int UINT32;
 typedef int INT32;
 typedef int BOOL;
 typedef unsigned char UINT8;
 typedef char INT8
+typedef unsigned short UINT16;
+typedef short INT16
 
 #define TRUE (1)
 #define FALSE (0)
@@ -15,6 +20,58 @@ typedef char INT8
 
 #define MPPP_DP_HDR_FLAG_BEGIN (1 << 7)
 #define MPPP_DP_HDR_FLAG_END   (1 << 6)
+
+typedef struct nBufBaseInfo_s {
+    struct nbuffer *PktNext;/* 报文链next */
+    struct nbuffer *pktPrev;/* 报文链prev */
+    INT8 *bufPtr;/* buf缓冲区的位置，用于分配和释放 */
+    INT8 *pData;/* 报文头位置 */
+    UINT16 pktLen;/* 报文的长度信息 */
+    UINT16 descFlags;
+    UINT16 linkPktType;
+}nBufBaseInfo_t
+
+typedef struct nBufInto_s {
+    nBufBaseInfo_t baseInfo;
+}nBufInfo_t;
+
+typedef struct nbuffer {
+    nBufInfo_t info;
+}nBuf_t;
+
+#define NBUF_ADJUST_PKTLEN(n, l)        (((nBuf_t*)(n))->info.baseInfo.pktLen += (l))
+#define NBUF_GET_PKTLEN(n)              (((nBuf_t*)(n))->info.baseInfo.pktLen)
+#define NBUF_SET_PKTLEN(n, l)           (((nBuf_t*)(n))->info.baseInfo.pktLen = (l))
+#define NBUF_SET_PKTNEXT(n, next)       (((nBuf_t*)(n))->info.baseInfo.pktNext = (next)))
+#define NBUF_SET_PKTPREV(n, prev)       (((nBuf_t*)(n))->info.baseInfo.pktPrev = (prev)))
+
+#define NBUF_ADJUST_PDATA(n, l)         (((nBuf_t*)(n))->info.baseInfo.pData += (l))
+#define NBUF_GET_PDATA(n)               (((nBuf_t*)(n))->info.baseInfo.pData)
+#define NBUF_SET_PDATA(n, p)            (((nBuf_t*)(n))->info.baseInfo.pData = (INT8 *)(p))
+
+#define NBUF_DESC_GET_DESCFLAGS(n)      (((nBuf_t*)(n))->info.baseInfo.descFlags)
+#define NBUF_DESC_SET_DESCFLAGS(n, v)   (((nBuf_t*)(n))->info.baseInfo.descFlags = (v))
+#define NBUF_DESC_SET_DESCFLAGS(n, flg) NBUF_DESC_SET_DESCFLAGS(n, NBUF_DESC_GET_DESCFLAGS(n) | (flg))
+#define NBUF_DESC_CLR_DESCFLAGS(n, flg) NBUF_DESC_SET_DESCFLAGS(n, NBUF_DESC_GET_DESCFLAGS(n) | (~(flg)))
+
+#define NBUF_DESC_GET_LINKTYPE(n)       (((nBuf_t*)(n))->info.baseInfo.linkPktType)
+
+#define NBUF_JOIN_TAIL(n, chainTail) {\
+    NBUF_SET_PKTNEXT(chainTail, n);\
+    NBUF_SET_PKTPREV(n, chainTail);\
+    (chainTail) = (n);\
+}
+
+#define NBUF_CHAIN_FOR_EACH(head, nbuf, next) \
+    for ((nbuf) = (head); \
+        (nbuf) && ({(next) = NBUF_GET_PKTNEXT(nbuf); 1;});\
+        (nbuf) = (next))
+
+
+#define NBUF_DESC_COPY(dst, src) 
+
+#define DFP_DESC_DESCFLAGS_MPPP_FRAG_BEGIN  (1 << 0)
+#define DFP_DESC_DESCFLAGS_MPPP_ENC         (1 << 1)
 
 typedef struct MPPP_DP_ENTRY_STRUCT {
     UINT32 pppIndex;
@@ -42,6 +99,13 @@ static void mpppDpFwdTxEncapHdr
     UINT32 seqNo = 0;
     INT8 *cp = sendPktData;
     UINT8 mpppHdrFlag = 0;
+    /**
+     * 根据mppp的协商逻辑，报文头部的格式可能有如下几种：
+     * ff 03 00 3d   普通报文
+     * ff 03 3d      pc报文
+     * 00 3d         ac报文
+     * 3d            acpc报文
+     * */
 
     if (cp[0] == 0xff) {
         cp += 2;
@@ -109,30 +173,27 @@ static BOOL mpppDpFwdTxFragPkt
         *encapOutPkt = NULL;
         return FALSE;
     }
-
-	//跳过链路头部的 ff 03
-    hdrLen -= 2;
 	
     //得到原始报文的长度
-    pktLen = NBUF_DESC_GET_PKTLEN(pPkt);
+    pktLen = NBUF_GET_PKTLEN(pPkt);
 	
-	//调整原始报文的长度和数据指针
-    NBUF_ADJUST_PKTLEN(pPkt, hdrLen);
-	NBUF_ADJUST_PKTPTR(pPkt, -hdrLen);
+	//调整原始报文的长度和数据指针，跳过链路头部信息
+    NBUF_ADJUST_PKTLEN(pPkt, -hdrLen);
+	NBUF_ADJUST_PDATA(pPkt, hdrLen);
     pktLen -= hdrLen;
 
     while (pktLen != 0) {
         fragWeight = MIN(fragWeight, pktLen);
-        pktFrag = nBufLenAlloc(fragWeight + hdrLen + NBUF_PREDATA_SIZE + ADJ_LINKINFO_LEN);
+        pktFrag = nBufLenAlloc(fragWeight + hdrLen + NBUF_PREDATA_SIZE);
         if (pktFrag == NULL) {
             *encapOutPkt = NULL;
             nBufChainFree(pHead);
             return FALSE;
         }
 
-        pData = NBUF_DESC_GET_PDATA(pktFrag);
+        pData = NBUF_GET_PDATA(pktFrag);
         NBUF_DESC_COPY(pktFrag, pPkt);//拷贝报文描述符，因此需要提前备份数据指针
-        NBUF_DESC_SET_PDATA(pktFrag, pData);
+        NBUF_SET_PDATA(pktFrag, pData);
         
         NBUF_DESC_CLR_DESCFLAGS(pktFrag, DFP_DESC_DESCFLAGS_MPPP_FRAG_BEGIN);//清楚报文描述符中的标记
         NBUF_SET_PKTNEXT(pktFrag, NULL);
@@ -143,14 +204,14 @@ static BOOL mpppDpFwdTxFragPkt
          * 拷贝指定长度的报文信息
          * 设置报文长度
         */
-        NBUF_ADJUST_PKTPTR(pktFrag, NBUF_PREDATA_SIZE);
-        memcpy(NBUF_DESC_GET_PDATA(pktFrag), linkHead, hdrLen);
-        memcpy(NBUF_DESC_GET_PDATA(pktFrag) + hdrLen, NBUF_DESC_GET_PDATA(pPkt), fragWeight);
-        NBUF_DESC_SET_PKTLEN(pktFrag, fragWeight + hdrLen);
+        NBUF_ADJUST_PDATA(pktFrag, NBUF_PREDATA_SIZE);
+        memcpy(NBUF_GET_PDATA(pktFrag), linkHead, hdrLen);
+        memcpy(NBUF_GET_PDATA(pktFrag) + hdrLen, NBUF_DESC_PDATA(pPkt), fragWeight);
+        NBUF_SET_PKTLEN(pktFrag, fragWeight + hdrLen);
 
-        //对原始报文进行调整
-        NBUF_ADJUST_PKTPTR(pPkt, fragWeight);
+        //对原始报文进行调整        
         NBUF_ADJUST_PKTLEN(pPkt, -fragWeight);
+        NBUF_ADJUST_PDATA(pPkt, fragWeight);
         pktLen -= fragWeight;
         if (NULL == pHeadTmp) {
             pHead = pHeadTmp = pktFrag;
@@ -180,17 +241,15 @@ static BOOL mpppDpFwdTxEncapPkt
     UINT32 pktLen;
     BOOL isInputOrder;
     BOOL isFragPkt;
-    BOOL isNeedSpinLock = TRUE;
     BOOL ret;
+    BOOL endPkt = FALSE;
     nBuf_t *pHead, *pktBuf, *pNext;
 
     pktLen = NBUF_DESC_GET_PKTLEN(pPkt);
     
     fragWeight = pMpppEntry->sendWeight;
     
-    isInputOrder = pMpppEntry->cfgFlag & MPPP_FLAG_INPUT_ORDER ? TRUE : FALSE;
-
-    if (unlikely(pktLen <= fragWeight)) {
+    if (likely(pktLen <= fragWeight)) {
         pHead = pPkt;
         isFragPkt = FALSE:
     } else {
@@ -198,7 +257,7 @@ static BOOL mpppDpFwdTxEncapPkt
         ret = mpppDpFwdTxFragPkt(pMpppEntry, pPkt, &pHead, fragWeight);
     }
 
-    if (isNeedSpinLock) {
+    if (isFragPkt) {
         MPPP_DP_TX_LOCK(pMpppEntry->sendLock);
     }
 
@@ -210,12 +269,13 @@ static BOOL mpppDpFwdTxEncapPkt
         NBUF_DESC_SET_DESCFLAGS(pktBuf, DFP_DESC_DESCFLAGS_MPPP_ENC);
     }
 
-    if (isNeedSpinLock) {
+    if (isFragPkt) {
         MPPP_DP_TX_UNLOCK(pMpppEntry->sendLock);
     }
 
 end:
     if (TRUE == isFragPkt) {
+        //如果是分片报文，则还需要释放原报文
         nBufFree(pPkt);
     }
 
